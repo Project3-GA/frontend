ae1b7a910c985668ba7176b766f2a4bd0f583142
jkim057 <joshkim057@gmail.com>
2021-06-10 12:32:14 -0400

    Component base setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
 import Form from './Form';
 import Cardform from './Cardform';
+
 function App() {
+  
 	const [loginForm, setLoginForm] = useState({ email: '', password: '' });
+
 	const [user, setUser] = useState({
 		email: '',
 		password: '',
 	});
+
 	return (
 		<main>
 			<h1>React Forms</h1>
@@ -17,36 +21,7 @@ function App() {
 				setLoginForm={setLoginForm}
 			/>
 			<Cardform loginForm={loginForm} />
-			{/* <form onSubmit={handleSubmit}>
-        <label htmlFor="email">Email: </label>
-        <input 
-          type="email" 
-          required 
-          placeholder="enter your email" 
-          name="email"
-          id="email"
-          value={email}
-          onChange={(event) => setEmail(event.target.value)}
-          />
-        <label htmlFor="longtext">Long Text</label>
-        <textarea 
-          name="longtext" 
-          id="longtext" 
-          cols="30" 
-          rows="10" 
-          value={longtext} onChange={event => setLongtext(event.target.value)}></textarea>
-        <select 
-          name="select" 
-          id="select"
-          value={select}
-          onChange={event => setSelect(event.target.value)}
-        >
-          <option value="mon">Monday</option>
-          <option value="wed">Wednesday</option>
-          <option value="fri">Friday</option>
-        </select>
-        <button type="submit">submit</button>
-      </form> */}
+			
 		</main>
 	);
 }
